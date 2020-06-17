09bcf206a810d069c004533e824d052c50eb5e3e
mishael254 <mishaelmishantoz@gmail.com>
2020-06-17 10:06:16 +0300

    remove import from the Quote folder inside the quote form

--- quotes/src/app/quote-form/quote-form.component.ts
+++ quotes/src/app/quote-form/quote-form.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+  
 
   constructor() { }
 
