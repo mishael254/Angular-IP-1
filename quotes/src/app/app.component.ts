2a75a6aeb159fd986e5dc5435768c9402f0286ce
mishael254 <mishaelmishantoz@gmail.com>
2020-06-17 11:43:00 +0300

    move quotes

--- quotes/src/app/app.component.ts
+++ quotes/src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { Quote } from "./quote";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes:Quote[]=[
-    {name:'The purpose of our lives is to be happy.'},
-    {name:'Life is what happens when you’re busy making other plans.'},
-    {name:'Get busy living or get busy dying.'},
-    {name:'You only live once, but if you do it right, once is enough.'},  
-    {name:'Many of life’s failures are people who did not realize how close they were to success when they gave up.'}
-  ]
+  
 }
